b2179f10f4412b459864fc0078d90f3d8d889e31
Irska28 <iiro.kaulio@gmail.com>
2025-12-11 13:45:08 +0200

    min ja max price

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,10 @@ function App() {
   const [currentState, setcurrentState] = useState('home');
   const [longestDownwardDays, setLongestDownwardDays] = useState(null);
 
+  const [maxPriceInfo, setMaxPriceInfo] = useState(null);
+  const [minPriceInfo, setMinPriceInfo] = useState(null)
+
+
   const findLongestDownwardTrend = (prices) => {
     if (!prices || prices.length === 0) return 0;
 
@@ -80,81 +84,125 @@ function App() {
 
         const downwardDays = findLongestDownwardTrend(data.prices);
         setLongestDownwardDays(downwardDays);
+
+
+
+        if (data.prices && data.prices.length > 0) {
+          const maxPriceData = data.prices.reduce((max, current) =>
+            current[1] > max[1] ? current : max
+          );
+          setMaxPriceInfo({
+            date: new Date(maxPriceData[0]).toLocaleString(),
+            price: maxPriceData[1]
+          });
+        }
+
+
+        if (data.prices && data.prices.length > 0) {
+          const minPriceData = data.prices.reduce((min, current) =>
+            current[1] < min[1] ? current : min
+          );
+          setMinPriceInfo({
+            date: new Date(minPriceData[0]).toLocaleString(),
+            price: minPriceData[1]
+          });
+        }
       })
       .catch((err) => console.error(err));
+
+
   };
 
   return (
     <div className="page">
       <div className="container">
-      <h1>Bitcoin Prices</h1>
+        <h1>Bitcoin Prices</h1>
 
 
-      <div className="dates">
-        <label>
-          From:
-          <input
-            type="date"
-            value={fromTime}
-            max={new Date().toISOString().split('T')[0]}
-            onChange={(e) => setFromTime(e.target.value)}
-          />
-        </label>
+        <div className="dates">
+          <label>
+            From:
+            <input
+              type="date"
+              value={fromTime}
+              max={new Date().toISOString().split('T')[0]}
+              onChange={(e) => setFromTime(e.target.value)}
+            />
+          </label>
 
-        <label>
-          To:
-          <input
-            type="date"
-            value={toTime}
-            max={new Date().toISOString().split('T')[0]}
-            onChange={(e) => setToTime(e.target.value)}
-          />
-        </label>
+          <label>
+            To:
+            <input
+              type="date"
+              value={toTime}
+              max={new Date().toISOString().split('T')[0]}
+              onChange={(e) => setToTime(e.target.value)}
+            />
+          </label>
 
 
-      </div>
-      <div className="center">
-        <button onClick={fetchPrices}>Get Prices</button>
-      </div>
-
-      {bitcoin.length > 0 && <BitcoinChart data={bitcoin} />}
-
-      {longestDownwardDays !== null && (
-        <div className="volume-info">
-          <h3>Longest Downward Trend</h3>
-          {longestDownwardDays === 0 ? (
-            <p>No downward trends during these dates.</p>
-          ) : (
-            <p><strong>Days:</strong> {longestDownwardDays}</p>
-          )}
         </div>
-      )}
-
-      {highestVolumeInfo && (
-        <div className="volume-info">
-          <h3>Highest Trading Volume</h3>
-          <p><strong>Date:</strong> {highestVolumeInfo.date}</p>
-          <p><strong>Volume:</strong> €{highestVolumeInfo.volume.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+        <div className="center">
+          <button onClick={fetchPrices}>Get Prices</button>
         </div>
-      )}
-      <div className="center">
-        <button onClick={() => setcurrentState('bitcoin')} style={{ cursor: 'pointer' }}>All price changes</button>
-      </div>
 
-      {currentState === 'bitcoin' && (
-        <div>
-          <div className="center">
-            <button onClick={() => setcurrentState('home')} style={{ cursor: 'pointer', marginBottom: '10px' }}>Close price changes</button>
+        {bitcoin.length > 0 && <BitcoinChart data={bitcoin} />}
+
+        {longestDownwardDays !== null && (
+          <div className="volume-info">
+            <h3>Longest Downward Trend</h3>
+            {longestDownwardDays === 0 ? (
+              <p>No downward trends during these dates.</p>
+            ) : (
+              <p><strong>Days:</strong> {longestDownwardDays}</p>
+            )}
           </div>
-          <ul>
-            {bitcoin.map(([timestamp, price], index) => (
-              <li key={index}>
-                {new Date(timestamp).toLocaleString()} : €{price.toFixed(2)}
-              </li>
-            ))}
-          </ul>
+        )}
+
+        {highestVolumeInfo && (
+          <div className="volume-info">
+            <h3>Highest Trading Volume</h3>
+            <p><strong>Date:</strong> {highestVolumeInfo.date}</p>
+            <p><strong>Volume:</strong> €{highestVolumeInfo.volume.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+
+          </div>
+        )}
+
+        {maxPriceInfo && (
+          <div className="max-info">
+            <h3>Max price</h3>
+            <p><strong>Max price:</strong> {maxPriceInfo.price}</p>
+            <p><strong>Date:</strong>{maxPriceInfo.date}</p>
+          </div>
+        )}
+
+
+        {minPriceInfo && (
+          <div className="max-info">
+            <h3>Min price</h3>
+            <p><strong>Min price: </strong> {minPriceInfo.price}</p>
+            <p><strong>Date: </strong> {minPriceInfo.date}</p>
+          </div>
+        )}
+        <div className="center">
+          <button onClick={() => setcurrentState('bitcoin')} style={{ cursor: 'pointer' }}>All price changes</button>
         </div>
-      )}
+
+        {currentState === 'bitcoin' && (
+          <div>
+            <div className="center">
+              <button onClick={() => setcurrentState('home')} style={{ cursor: 'pointer', marginBottom: '10px' }}>Close price changes</button>
+            </div>
+            <ul>
+              {bitcoin.map(([timestamp, price], index) => (
+                <li key={index}>
+                  {new Date(timestamp).toLocaleString()} : €{price.toFixed(2)}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
 
 
       </div>
